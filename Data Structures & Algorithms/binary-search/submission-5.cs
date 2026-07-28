32a93f178f8edbde35a05278706f5a5f71526681
yacinekahlis <80855347+yacinekahlis@users.noreply.github.com>
2026-07-28 11:13:22 -0400

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.Length - 1;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] == target) return mid;
+            //la target est dans la partie de droite et on s'est que mid n'est pas la bonne reponse
+            if(nums[mid]<target) low=mid+1;
+            //la target est dans la partie de gauche et on s'est que mid n'est pas la bonne reponse
+            else high=mid-1;
+        }
+
+        return -1;
+    }
+}
