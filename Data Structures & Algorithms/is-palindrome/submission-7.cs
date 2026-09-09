153d2e1cd99daedad1fb65dd72758e5c76e83402
yacinekahlis <80855347+yacinekahlis@users.noreply.github.com>
2026-09-08 21:01:17 -0400

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        int left = 0;
+        int right = s.Length-1;
+
+        while(left<right){
+            while(left<right && !char.IsLetterOrDigit(s[left])) left++;
+            while(left<right && !char.IsLetterOrDigit(s[right])) right--;
+
+            if (char.ToLower(s[left]) != char.ToLower(s[right])) return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+
+    }
+}
